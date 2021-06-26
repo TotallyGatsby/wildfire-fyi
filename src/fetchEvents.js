48ec436a55f4cccbf28ef46b9202115f748c4b18
TotallyGatsby <700673+TotallyGatsby@users.noreply.github.com>
2021-06-25 22:24:50 -0700

    Initial fire fetching API, parses out some fire data to be stored later.

---
+++ src/fetchEvents.js
@@ -0,0 +1,71 @@
+const fetch = require('node-fetch');
+
+const fireState = 'US-WA';
+const daysBack = 14;
+
+// Grab some attributes from the fire
+function parseArcGisFire(fire) {
+  let parsedFire = {};
+
+  // Location
+  parsedFire.latitude = fire.attributes.InitialLatitude;
+  parsedFire.longitude = fire.attributes.InitialLongitude;
+
+  // Timeline
+  parsedFire.startTime = fire.attributes.FireDiscoveryDateTime;
+  parsedFire.lastUpdate = fire.attributes.ModifiedOnDateTime_dt;
+  parsedFire.containmentTime = fire.attributes.ContainmentDateTime;
+  parsedFire.controlTime = fire.attributes.ControlDateTime;
+  parsedFire.outTime = fire.attributes.FireOutDateTime;
+
+  // Metadata
+  parsedFire.uniqueFireId = fire.attributes.UniqueFireIdentifier;
+  parsedFire.incidentName = fire.attributes.IncidentName;
+  parsedFire.globalUID = fire.attributes.GlobalID;
+
+  // Details
+  parsedFire.discoveryAcres = fire.attributes.DiscoveryAcres;
+  parsedFire.dailyAcres = fire.attributes.DailyAcres;
+  parsedFire.initialResponseAcres = fire.attributes.InitialResponseAcres;
+
+  parsedFire.causeType = fire.attributes.FireCause;
+  parsedFire.causeDetail = fire.attributes.FireCauseGeneral;
+  parsedFire.causeSubDetail = fire.attributes.FireCauseSpecific;
+
+
+  return parsedFire;
+}
+
+function constructArcGisUrl() {
+  let previousDate = new Date();
+  previousDate.setDate(previousDate.getDate() - daysBack);
+  console.log(previousDate);
+  return encodeURIComponent(`POOState = '${fireState}' AND ModifiedOnDateTime_dt >= TIMESTAMP '${previousDate.getMonth() + 1}-${previousDate.getDate()}-${previousDate.getFullYear()} 0:00:0'`);
+}
+
+export async function handler() {
+  let features;
+
+  await fetch(`https://services3.arcgis.com/T4QMspbfLg3qTGWY/arcgis/rest/services/CY_WildlandFire_Locations_ToDate/FeatureServer/0/query?where=${constructArcGisUrl()}&outFields=*&outSR=4326&f=json`)
+    .then(res => res.json())
+    .then(json => {
+      features = json.features;
+    })
+    .catch(err => {
+      console.log(err);
+
+      return {
+        statusCode: 500,
+        headers: { "Content-Type": "text/plain" },
+        body: `Failure.`,
+      };
+    });
+
+  let fires = features.map(parseArcGisFire);
+
+  return {
+    statusCode: 200,
+    headers: { "Content-Type": "text/plain" },
+    body: `${JSON.stringify(fires[0])}`,
+  };
+}
